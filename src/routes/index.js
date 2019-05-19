af01b7767f0f72c87fd9afdbbb83f4382c0b880c
Ghenet Kibrom <ghenetkibrom@Ghenets-MacBook-Pro.local>
2019-05-19 13:16:38 -0700

    adds the children routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import {Route, IndexRoute} from 'react-router'
 import Template from '../containers/Template'
+import Home from '../containers/Home'
+import Profile from '../containers/Profile'
 
 const createRoutes = () => {
     return (
-      <Route path='/' component={Template}></Route>
+      <Route path='/' component={Template}>
+          <IndexRoute component={Home} />
+          <Route path={'/profile'} component={Profile} />
+      </Route>
     )
 }
 
